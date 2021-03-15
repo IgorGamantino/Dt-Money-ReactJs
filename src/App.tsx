7d16c57ec43124c44655f950001be5aedd1d198b
Igor Amantino <justin.amantino@gmail.com>
2021-03-15 14:38:36 -0300

    estilizaçao global

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-
+import {GlobalStyle} from './styles/global';
 export function App() {
   return (
     <div className="App">
         <h1>Hello Word</h1>
+        <GlobalStyle />
     </div>
   );
 }
